fc1df1d14d0d00c8f4fcd0ea481a4f618569de99
Ollie Poole <oliverpoole7@gmail.com>
2019-11-17 01:37:28 +0200

    Update frameworks

---
+++ .build/x86_64-apple-macosx/debug/CNIOLinux.build/ifaddrs-android.c.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/oliverpoole/Development\ Projects/Hackathons/Parker/backend-better/parker/.build/checkouts/swift-nio/Sources/CNIOLinux/ifaddrs-android.c
--- Package.swift
+++ Package.swift
@@ -1,11 +1,10 @@
-// swift-tools-version:5.0
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "parker",
     dependencies: [
-        // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", .upToNextMinor(from: "3.3.0")),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
     ],
     targets: [
         .target(name: "App", dependencies: ["Vapor"]),
